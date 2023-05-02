863dec1de2ea27306dbddb12c4d8ff292c65f768
wangchengcheng <ccxhwmy@gmail.com>
2023-05-02 11:25:59 +0800

    update

---
+++ getid.c
@@ -0,0 +1,45 @@
+#define _GNU_SOURCE
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <sched.h>
+#include <signal.h>
+#include <unistd.h>
+
+/* 定义一个给 clone 用的栈，栈大小1M */
+#define STACK_SIZE (1024 * 1024)
+static char container_stack[STACK_SIZE];
+
+char* const container_args[] = {
+    "/bin/bash",
+    NULL
+};
+
+int container_main(void* arg)
+{
+    printf("Container [%5d] - inside the container!\n", getpid());
+    sethostname("container",10);
+    /* 重新mount proc文件系统到 /proc下 */
+    system("mount -t proc proc /proc");
+    execv(container_args[0], container_args);
+    printf("Something's wrong!\n");
+    return 1;
+}
+
+int main()
+{
+    printf("Parent - start a container!\n");
+
+    printf("Parent: eUID = %ld, eGID = %ld, UID=%ld, GID=%ld\n",
+            (long) geteuid(), (long) getegid(), (long) getuid(), (long) getgid());
+
+    int container_pid = clone(container_main, container_stack + STACK_SIZE, 
+            CLONE_NEWUTS | CLONE_NEWIPC | CLONE_NEWPID | CLONE_NEWNS | SIGCHLD, NULL); /*启用CLONE_NEWUTS Namespace隔离 */
+    
+    printf("Parent [%5d] - Container [%5d]!\n", getpid(), container_pid);
+
+    waitpid(container_pid, NULL, 0);
+    printf("Parent - container stopped!\n");
+    system("mount -t proc proc /proc");
+    return 0;
+}
